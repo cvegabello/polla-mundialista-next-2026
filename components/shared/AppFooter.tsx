32de8ef7438199de129679911371611268f6fcba
cvegabello <carlosvegabello@gmail.com>
2026-06-20 01:17:11 -0400

    chore: bump footer version to 3.1

--- components/shared/AppFooter.tsx
+++ components/shared/AppFooter.tsx
@@ -7,7 +7,7 @@ interface AppFooterProps {
 
 export const AppFooter = ({ lang = "es" }: AppFooterProps) => {
   // 👇 Aquí controla la versión manualmente
-  const APP_VERSION = "PROD v3.0";
+  const APP_VERSION = "PROD v3.1";
 
   // 👇 Aquí pone el nuevo nombre oficial de su app
   const APP_NAME = "TiqueBet";
